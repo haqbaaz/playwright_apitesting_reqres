ef1a25fc1330b3cfc3c73c282051f556c078e09b
Md Haqbaaz <mdhaqbaaz@gmail.com>
2026-05-23 12:25:26 +0530

    first commit for api testing from demo API's reqres.com

---
+++ .env.pat
@@ -0,0 +1,6 @@
+API_TOKEN=d9791ec45d6c5b35edfd8d6125031738f2d249f52b8dbf466305f167793f9b79
+BASE_URL=https://gorest.co.in
+NAME=jules_hathaway
+GENDER=Male
+EMAIL=john.marshall@example.com
+STATUS=active
---
+++ tests/api_testing.spec.ts
@@ -0,0 +1,75 @@
+import { test, expect, request } from '@playwright/test';
+
+
+test.describe.serial("api requests", () => {
+
+    var userId: number;
+
+    test('api Request', async ({ request }) => {
+
+        const res = await request.get('/public/v2/users/', {
+
+        })
+        console.log(await res.json())
+        console.log(await res.status())
+        await expect(res.status()).toBe(200)
+    })
+
+    test('post api', async ({ request }) => {
+
+        const response = await request.post('/public/v2/users/', {
+
+            data: {
+                name: process.env.NAME,
+                gender: process.env.GENDER,
+                email: process.env.EMAIL,
+                status: process.env.STATUS
+            },
+
+        })
+
+        console.log(await response.json())
+        console.log(await response.status())
+        const postData = await response.json();
+        userId = postData.id;
+
+        console.log(userId);
+        await expect(response.status()).toBe(201)
+
+
+    })
+
+    test('geting users', async ({ request, }) => {
+
+        const res = await request.get(`/public/v2/users/${userId}`, {
+
+        })
+
+        console.log(await res.json())
+        console.log(await res.status())
+        await expect(res.status()).toBe(200)
+    })
+
+    test('deleting user', async ({ request, }) => {
+
+        const res = await request.delete(`/public/v2/users/${userId}`, {
+
+        })
+
+        console.log(await res.status())
+        await expect(res.status()).toBe(204)
+    })
+
+    test('geting user', async ({ request, }) => {
+
+        const res = await request.get(`/public/v2/users/${userId}`, {
+
+        })
+
+        console.log(await res.json())
+        console.log(await res.status())
+        await expect(res.status()).toBe(404)
+    })
+
+
+})
